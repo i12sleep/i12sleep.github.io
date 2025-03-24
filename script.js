463f452cb2506ccb93b1f0ab78dec79ff8567c27
Brandon Sirimat <bsirimat@outlook.com>
2025-03-25 01:05:14 +1100

    improved look and functionality

--- script.js
+++ script.js
@@ -1,6 +1,2 @@
-//the math for calculating probabilities relies on the fact that
-//each unit has a certain amount of number in the combined pool
-//and that there are only a certain amount of units
-//and that there are 5 possible chances to get them
-
+//empty
 
